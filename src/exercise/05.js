ef2bf3afb4e18c41b3e7ace7f5f229655e9a6c9e
Asha J S <94jsasha@gmail.com>
2020-10-20 11:20:38 +0200

    Add exercise 05 extra-2

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,28 +4,29 @@
 import React from 'react'
 import '../box-styles.css'
 
-function Box({ className = '', style, ...otherProps }) {
+function Box({ className = '', size, style, ...otherProps }) {
+  const sizeClass = size ? `box--${size}` : ''
   return (<div
-    className={`box ${className}`}
+    className={`box ${className} ${sizeClass}`}
     style={{ fontStyle: 'italic', ...style }}
     {...otherProps} />)
 }
 
 
 const smallBox = <Box
-  className=" box--small"
+  size="small"
   style={{ backgroundColor: 'lightblue' }}>
   small lightblue box
   </Box>
 
 const mediumBox = <Box
-  className=" box--medium"
+  size="medium"
   style={{ backgroundColor: 'pink' }} >
   medium pink box
   </Box>
 
 const largeBox = <Box
-  className="box--large"
+  size="large"
   style={{ backgroundColor: 'orange' }} >
   large orange box
    </Box>
